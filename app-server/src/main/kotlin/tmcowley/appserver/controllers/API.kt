094d82d3861acb652fda9a94faed46d88c277d29
tmcowley <thomas.cowley19@gmail.com>
2022-02-13 13:16:09 +0000

    cleanup APIs, prototype files

--- app-server/src/main/kotlin/tmcowley/appserver/controllers/API.kt
+++ app-server/src/main/kotlin/tmcowley/appserver/controllers/API.kt
@@ -1,106 +1,103 @@
 package tmcowley.appserver.controllers
 
-import tmcowley.appserver.Singleton;
-
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.http.MediaType.*
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import tmcowley.appserver.Singleton
 import tmcowley.appserver.objects.Key
 import tmcowley.appserver.objects.KeyPair
 import tmcowley.appserver.utils.Langtool
-import tmcowley.appserver.structures.DataStructures;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-import org.springframework.cache.annotation.Cacheable;
-
-
-import org.springframework.http.MediaType.*;
-
-
 
 // https://kotlinlang.org/docs/annotations.html#arrays-as-annotation-parameters
 @RestController
-@CrossOrigin(
-    origins = ["http://localhost:3000"]
-)
+@CrossOrigin(origins = ["http://localhost:3000"])
 @RequestMapping(
-    value=["/api"],
-    consumes=["text/plain"]
-    // consumes=["application/json"]
-)
+        value = ["/api"],
+        consumes = ["text/plain"]
+        // consumes=["application/json"]
+        )
 class API {
 
     init {}
 
     @Cacheable()
-    @PostMapping(
-        value=["/submit"],
-        consumes=["text/plain"]
-    )
+    @PostMapping(value = ["/submit"], consumes = ["text/plain"])
     fun submit(@RequestBody input: String): Array<String> {
+        println("\n\n/submit endpoint called")
+        return submitSentence(input)
+    }
 
-        println("\n\n/submit endpoint called");
+    @Cacheable()
+    @PostMapping(value = ["/convert/rhs"])
+    fun convertToRHS(@RequestBody input: String?): String {
+        // for each alphabetic char in string -> lookup keypair, get right key in keypair
+        return convertFullToRHS(input)
+    }
 
-        val lowercaseInput = input.lowercase();
+    @Cacheable()
+    @PostMapping(value = ["/convert/lhs"])
+    fun convertToLHS(@RequestBody input: String?): String {
+        // for each alphabetic char in string -> lookup keypair, get right key in keypair
+        return convertFullToLHS(input)
+    }
+
+    fun submitSentence(input: String): Array<String> {
+        val lowercaseInput = input.lowercase()
 
         // create input word array
-        val inputWords: Array<String> = splitIntoWords(lowercaseInput);
+        val inputWords: Array<String> = splitIntoWords(lowercaseInput)
 
-        val resultingSentences: MutableList<String> = buildAndReadTrees(inputWords);
+        val resultingSentences: MutableList<String> = buildAndReadTrees(inputWords)
 
         // report input and output
         run {
-            System.out.println("\nInput: ${input}");
-            System.out.println("\nResults: ");
+            System.out.println("\nInput: ${input}")
+            System.out.println("\nResults: ")
 
             if (resultingSentences.isEmpty()) {
-                System.out.println("Notice: system found no resulting sentences");
+                System.out.println("Notice: system found no resulting sentences")
             }
 
             for (sentence: String in resultingSentences) {
-                System.out.println(sentence);
+                System.out.println(sentence)
             }
         }
 
         // filter ungrammatical and rank viable
         run {
-            resultingSentences.sortWith( StringComparator );
+            resultingSentences.sortWith(SentenceComparator)
 
-            var i = 0;
+            var i = 0
             for (sentence: String in resultingSentences) {
-                println("i: ${i}");
-                println("sentence: ${sentence}");
+                println("i: ${i}")
+                println("sentence: ${sentence}")
                 println("error count: ${Singleton.langtool.countErrors(sentence)}")
                 println()
-                i++;
+                i++
             }
 
-            // pick top 5 
+            // pick top 5
             // ...
         }
 
-
-        return resultingSentences.toTypedArray();
+        return resultingSentences.toTypedArray()
     }
 
     fun splitIntoWords(sentence: String): Array<String> {
-        return sentence.split(" ").toTypedArray();
+        return sentence.split(" ").toTypedArray()
     }
 
     fun buildAndReadTrees(inputWords: Array<String>): MutableList<String> {
 
-        var listOfMatchedWords: MutableList<MutableList<String>> = mutableListOf();
+        var listOfMatchedWords: MutableList<MutableList<String>> = mutableListOf()
 
         for (word: String in inputWords) {
 
-            var wordKeyPairs: MutableList<KeyPair> = mutableListOf();
+            var wordKeyPairs: MutableList<KeyPair> = mutableListOf()
 
             for (char: Char in word) {
 
@@ -108,19 +105,19 @@ class API {
                 if (isAlphabetic(char)) {
 
                     // get key for char
-                    val key: Key = Key(char);
+                    val key: Key = Key(char)
 
                     // get key-pair for key
-                    val keyPair: KeyPair? = Singleton.getKeyPair(key);
+                    val keyPair: KeyPair? = Singleton.getKeyPair(key)
 
                     // key-pair lookup fails
                     if (keyPair == null) {
                         println("Error: getKeyPair(${key.toString()}) failed")
-                        continue;
+                        continue
                     }
 
                     // add the key-pair to the current word
-                    wordKeyPairs.add(keyPair);
+                    wordKeyPairs.add(keyPair)
                 }
 
                 // char is non-alphabetic -> do nothing
@@ -130,142 +127,116 @@ class API {
 
             // store current word
 
-            var matchedWords: MutableList<String> = Singleton.structures.getWords(wordKeyPairs);
+            var matchedWords: MutableList<String> = Singleton.structures.getWords(wordKeyPairs)
 
             if (matchedWords.isEmpty()) {
-                println("\nwordKeyPairs: ${wordKeyPairs}");
-                println("Notice: the word '${word}' was not matched");
-                continue;
+                println("\nwordKeyPairs: ${wordKeyPairs}")
+                println("Notice: the word '${word}' was not matched")
+                continue
             }
 
             // add the viable words to the total list
-            listOfMatchedWords.add(matchedWords);
+            listOfMatchedWords.add(matchedWords)
         }
 
         // trees have been built, calculate viable sentences
 
         // no words have been computed
         if (listOfMatchedWords.isEmpty()) {
-            System.out.println("Notice: listOfMatchedWords is empty");
-            return mutableListOf();
+            System.out.println("Notice: listOfMatchedWords is empty")
+            return mutableListOf()
         }
 
         // compute viable sentences from text array
-        val resultingSentences: MutableList<String> = Singleton.structures.getSentences(listOfMatchedWords);
+        val resultingSentences: MutableList<String> =
+                Singleton.structures.getSentences(listOfMatchedWords)
 
-        return resultingSentences;
+        return resultingSentences
     }
 
     fun isAlphabetic(char: Char): Boolean {
-        return (char in 'a'..'z' || char in 'A'..'Z');
+        return (char in 'a'..'z' || char in 'A'..'Z')
     }
 
-    @Cacheable()
-    @PostMapping(value=["/convert/rhs"])
-    fun convertToRHS(@RequestBody input: String?): String {
-        // for each alphabetic char in string -> lookup keypair, get right key in keypair
-
+    fun convertFullToRHS(input: String?): String {
         if (input == null) {
-            return "";
+            return ""
         }
 
-        var inputRHS: String = "";
+        var inputRHS: String = ""
 
         for (char: Char in input) {
             if (isAlphabetic(char)) {
 
-                val key: Key = Key(char);
+                val key: Key = Key(char)
 
-                val keyPair: KeyPair? = Singleton.getKeyPair(key);
+                val keyPair: KeyPair? = Singleton.getKeyPair(key)
 
                 if (keyPair == null) {
-                    inputRHS += char;
-                    continue;
+                    inputRHS += char
+                    continue
                 }
 
-                val charRHS: Char = keyPair.rightKey.character;
+                val charRHS: Char = keyPair.rightKey.character
 
-                inputRHS += charRHS;
+                inputRHS += charRHS
 
-                continue;
+                continue
             }
 
-            inputRHS += char;
+            inputRHS += char
         }
 
-        return inputRHS;
+        return inputRHS
     }
 
-    @Cacheable()
-    @PostMapping(value=["/convert/lhs"])
-    fun convertToLHS(@RequestBody input: String?): String {
-
-        // for each alphabetic char in string -> lookup keypair, get right key in keypair
-
+    fun convertFullToLHS(input: String?): String {
         if (input == null) {
-            return "";
+            return ""
         }
 
-        var inputLHS: String = "";
+        var inputLHS: String = ""
 
         for (char: Char in input) {
             if (isAlphabetic(char)) {
 
-                val key: Key = Key(char);
+                val key: Key = Key(char)
 
-                val keyPair: KeyPair? = Singleton.getKeyPair(key);
+                val keyPair: KeyPair? = Singleton.getKeyPair(key)
 
                 if (keyPair == null) {
-                    inputLHS += char;
-                    continue;
+                    inputLHS += char
+                    continue
                 }
 
-                val charLHS: Char = keyPair.leftKey.character;
+                val charLHS: Char = keyPair.leftKey.character
 
-                inputLHS += charLHS;
+                inputLHS += charLHS
 
-                continue;
+                continue
             }
 
-            inputLHS += char;
+            inputLHS += char
         }
 
-        return inputLHS;
+        return inputLHS
     }
-
-    @PostMapping(value=["/test"])
-    fun getAll(@RequestBody body: kotlin.Any): String {
-        println("/api/test called");
-
-        // var body: String? = null;
-
-        try {
-            println("test post mapping collected: $body")
-        } catch (e: Exception) {
-            println("printing request body failed");
-        }
-
-        return "/api/test called"
-    }
-
 }
 
-class StringComparator {
-
-	companion object : Comparator<String> {
-
+class SentenceComparator {
+    companion object : Comparator<String> {
         val langtool: Langtool = Singleton.langtool
+        override fun compare(first: String, second: String): Int {
 
-		override fun compare(first: String, second: String): Int { 
+            // calculate syntax correctness scores
+            val firstScore: Int = langtool.countErrors(first)
+            val secondScore: Int = langtool.countErrors(second)
 
-            val firstScore: Int = langtool.countErrors(first);
-            val secondScore: Int = langtool.countErrors(second);
-            
             when {
                 // lower score is better
                 firstScore != secondScore -> return (secondScore - firstScore)
                 else -> return (0)
-		    }
+            }
         }
-	}
+    }
 }
